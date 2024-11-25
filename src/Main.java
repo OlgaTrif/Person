7ac7bb5f4764657808b3c4bf7eba67bff6d2e34c
trifonova_o_e <myowl@list.ru>
2024-11-25 11:49:20 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.*;
+
+/**
+ * Задание 1: Создайте класс Person с полями name и age. Реализуйте сериализацию и десериализацию этого класса в файл.
+ *
+ * Задание 2: Используя JPA, создайте базу данных для хранения объектов класса Person.
+ * Реализуйте методы для добавления, обновления и удаления объектов Person.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Ivan", 23);
+
+        try (FileOutputStream fos = new FileOutputStream("person.bin")) {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(person);
+            System.out.println("Успешно сериализован");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (FileInputStream fis = new FileInputStream("person.bin")) {
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            person = (Person)ois.readObject();
+            System.out.println("Успешно сериализован");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Успешно десериализован");
+        System.out.println("Имя: " + person.getName());
+        System.out.println("Возраст: " + person.getAge());
+        System.out.println();
+    }
+}
